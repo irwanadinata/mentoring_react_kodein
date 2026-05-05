c40046f4452e12b23b83661578ce8b09023d8f42
irwanadinata <irwanadinata9@gmail.com>
2026-05-05 11:35:09 +0700

    react pertemuan 1,2,3

---
+++ pertemuan-2/src/pages/App.jsx
@@ -0,0 +1,13 @@
+import '../style/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='judul'>Judul</div>
+      <p className='paragraft'>Paragraft</p>
+    </>
+  )
+}
+
+export default App;
---
+++ pertemuan-3/src/pages/App.jsx
@@ -0,0 +1,15 @@
+import "../styles/App.css";
+import Navbar from "../components/section/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-amber-600 text-3xl">Judul</h1>
+      <p className="text-2xl">Paragraft</p>
+      <a href="">Link</a>
+    </>
+  );
+}
+
+export default App;
