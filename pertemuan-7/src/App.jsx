861186a22abd9ed69e4fb966b78419c7f41450d7
irwanadinata <irwanadinata9@gmail.com>
2026-05-18 11:45:32 +0700

    pertemuan 7

---
+++ pertemuan-7/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  //useState
+  const [count, setCount] = useState(10);
+
+  //useEffect
+  useEffect(()=>{
+    //axios.get("url backend")
+  }, [])
+
+  return (
+    <>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count - 1)}>Minus</button>
+      <button onClick={() => setCount(count + 1)}>Plus</button>
+    </>
+  );
+}
+
+export default App;
